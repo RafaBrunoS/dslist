fff4c33e5f7b54c7daa27d4833d2de83653cbd42
Bruno Rafael <bruno.silva4@uscsonline.com.br>
2023-11-13 21:48:40 -0300

    Projeto created

---
+++ dslist/src/test/java/com/cursodeve/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodeve.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
